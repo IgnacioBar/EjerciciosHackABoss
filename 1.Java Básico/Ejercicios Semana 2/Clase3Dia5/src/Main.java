ce7d8bee7936b2ce8c46521d01cb7c670d45af13
Ignacio Barbeyto Madruga <ignacio.barbeyto@gmail.com>
2024-02-28 12:53:58 +0100

    Ejercicios Semana 2

--- 1.Java Básico/Ejercicios Semana 2/Clase3Dia5/src/Main.java
+++ 1.Java Básico/Ejercicios Semana 2/Clase3Dia5/src/Main.java
@@ -2,42 +2,36 @@ public class Main {
     public static void main(String[] args) {
         Verdura[] verduras = new Verdura[5];
 
-        verduras[0] = new Verdura(1, "Zanahoria", "Naranja", 41, false);
-        verduras[1] = new Verdura(2, "Espinaca", "Verde", 23, true);
-        verduras[2] = new Verdura(3, "Tomate", "Rojo", 18, false);
-        verduras[3] = new Verdura(4, "Brócoli", "Verde", 34, true);
-        verduras[4] = new Verdura(5, "Papa", "Marrón", 77, true);
+        verduras[0] = new Verdura(1, "Zanahoria", "Naranja", 50, false);
+        verduras[1] = new Verdura(2, "Lechuga", "Verde", 10, false);
+        verduras[2] = new Verdura(3, "Tomate", "Rojo", 20, false);
+        verduras[3] = new Verdura(4, "Brócoli", "Verde", 30, true);
+        verduras[4] = new Verdura(5, "Pimiento", "Rojo", 15, true);
 
-        System.out.println("\nVerduras:");
+        System.out.println("Verduras almacenadas:");
         for (Verdura verdura : verduras) {
-            System.out.println(verdura.getNombre() + " tiene " + verdura.getCalorias() + " calorías.");
+            System.out.println("Nombre: " + verdura.getNombre() + ", Calorías: " + verdura.getCalorias());
         }
 
-        System.out.println("\nAntes de los cambios:");
-        for (Verdura verdura : verduras) {
-            System.out.println(verdura);
-        }
+        verduras[0].setNombre("Cebolla");
+        verduras[0].setColor("Blanco");
+        verduras[0].setCalorias(25);
+        verduras[0].setDebeCocinarse(true);
 
-        verduras[1].setNombre("Lechuga");
-        verduras[1].setColor("Verde");
-        verduras[1].setCalorias(15);
-        verduras[1].setDebeCocinarse(false);
-
-        verduras[3].setNombre("Pepino");
+        verduras[3].setNombre("Espinaca");
         verduras[3].setColor("Verde");
-        verduras[3].setCalorias(16);
-        verduras[3].setDebeCocinarse(false);
+        verduras[3].setCalorias(15);
+        verduras[3].setDebeCocinarse(true);
 
-        System.out.println("Mostrando cambios realizados:");
+        System.out.println("\nVerduras después del cambio:");
         for (Verdura verdura : verduras) {
-            verdura.mostrarCambios();
+            System.out.println(verdura.toString());
         }
-
 
         System.out.println("\nVerduras de color verde:");
         for (Verdura verdura : verduras) {
             if ("Verde".equalsIgnoreCase(verdura.getColor())) {
-                System.out.println(verdura.getNombre() + " -> " + verdura.getColor());
+                System.out.println(verdura);
             }
         }
     }
--- 1.Java Básico/Ejercicios Semana 2/Clase3Dia5/src/Verdura.java
+++ 1.Java Básico/Ejercicios Semana 2/Clase3Dia5/src/Verdura.java
@@ -5,54 +5,12 @@ public class Verdura {
     private int calorias;
     private boolean debeCocinarse;
 
-    // Atributos para rastrear los cambios
-    private String nombreOriginal;
-    private String colorOriginal;
-    private int caloriasOriginal;
-    private boolean debeCocinarseOriginal;
-
     public Verdura(int id, String nombre, String color, int calorias, boolean debeCocinarse) {
         this.id = id;
         this.nombre = nombre;
         this.color = color;
         this.calorias = calorias;
         this.debeCocinarse = debeCocinarse;
-        guardarEstadoOriginal();
-    }
-
-    private void guardarEstadoOriginal() {
-        this.nombreOriginal = nombre;
-        this.colorOriginal = color;
-        this.caloriasOriginal = calorias;
-        this.debeCocinarseOriginal = debeCocinarse;
-    }
-
-    // Métodos setters modificados para no necesitar rastreo adicional aquí
-
-    // Método para mostrar cambios específicos
-    public void mostrarCambios() {
-        boolean cambio = false;
-
-        if (!nombre.equals(nombreOriginal)) {
-            System.out.println("Nombre cambiado de " + nombreOriginal + " a " + nombre + ".");
-            cambio = true;
-        }
-        if (!color.equals(colorOriginal)) {
-            System.out.println("Color cambiado de " + colorOriginal + " a " + color + ".");
-            cambio = true;
-        }
-        if (calorias != caloriasOriginal) {
-            System.out.println("Calorías cambiadas de " + caloriasOriginal + " a " + calorias + ".");
-            cambio = true;
-        }
-        if (debeCocinarse != debeCocinarseOriginal) {
-            System.out.println("DebeCocinarse cambiado de " + debeCocinarseOriginal + " a " + debeCocinarse + ".");
-            cambio = true;
-        }
-
-        if (!cambio) {
-            System.out.println("No hay cambios en " + nombre + ".");
-        }
     }
 
     public int getId() {
@@ -63,47 +21,33 @@ public class Verdura {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public int getCalorias() {
         return calorias;
     }
 
-    public boolean getDebeCocinarse() {
-        return debeCocinarse;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        if (!this.nombre.equals(nombre)) {
-            this.nombre = nombre;
-            guardarEstadoOriginal();
-        }
-    }
-
-    public void setColor(String color) {
-        if (!this.color.equals(color)) {
-            this.color = color;
-            guardarEstadoOriginal();
-        }
-    }
-
     public void setCalorias(int calorias) {
-        if (this.calorias != calorias) {
-            this.calorias = calorias;
-            guardarEstadoOriginal();
-        }
+        this.calorias = calorias;
     }
 
     public void setDebeCocinarse(boolean debeCocinarse) {
-        if (this.debeCocinarse != debeCocinarse) {
-            this.debeCocinarse = debeCocinarse;
-            guardarEstadoOriginal();
-        }
+        this.debeCocinarse = debeCocinarse;
+    }
+
+    @Override
+    public String toString() {
+        return "Verdura " + id + ", nombre = " + nombre + ", color = " + color +
+                ", calorias = " + calorias + ", ¿debe cocinarse? = " + debeCocinarse + ".";
     }
 }
