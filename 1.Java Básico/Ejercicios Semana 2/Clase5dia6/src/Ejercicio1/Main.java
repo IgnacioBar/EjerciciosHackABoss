cdc254bca2de29007d97ef0b6ff0bfb5f4cce574
Ignacio Barbeyto Madruga <ignacio.barbeyto@gmail.com>
2024-02-28 13:02:46 +0100

    Ejercicio 1 Clase5Dia6

---
+++ 1.Java Básico/Ejercicios Semana 2/Clase5dia6/src/Ejercicio1/Main.java
@@ -0,0 +1,19 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear objetos de cada clase hija
+        Mamifero mamifero = new Mamifero(1, "León", 5, "Pelaje corto", "Carnívoro",
+                4, "Vivíparo", "Amarillo", "Savana");
+        Ave ave = new Ave(2, "Águila", 3, "Plumas", "Carnívoro",
+                2.5, "Planeo", "Marrón", "Gancho");
+        Reptil reptil = new Reptil(3, "Serpiente", 2, "Escamas", "Carnívoro",
+                1.5, "Queratinosas", "Veneno neurotóxico", "Selva");
+
+        mamifero.saludar();
+        ave.saludar();
+        reptil.saludar();
+        Animal animal = new Mamifero(4, "Delfín", 7, "Piel lisa", "Piscívoro",
+                0, "Vivíparo", "Gris", "Océano");
+    }
+}
